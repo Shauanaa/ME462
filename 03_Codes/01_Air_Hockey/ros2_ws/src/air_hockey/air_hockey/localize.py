3b83a46f885b934361043155a40cc4d6bc976daf
tanacar2002 <acartancagatay@hotmail.com>
2024-06-30 12:02:44 +0300

    Added localization to python api

--- 03_Codes/01_Air_Hockey/ros2_ws/src/air_hockey/air_hockey/localize.py
+++ 03_Codes/01_Air_Hockey/ros2_ws/src/air_hockey/air_hockey/localize.py
@@ -17,8 +17,8 @@ class LocalizationNode(Node):
                                         families='tag36h11', #tagCircle49h12 , tag36h11
                                         nthreads=4,
                                         quad_decimate=1,
-                                        quad_sigma=0.25,
-                                        refine_edges=0,
+                                        quad_sigma=0.0,
+                                        refine_edges=1,
                                         decode_sharpening=0.25,
                                         # max_hamming=1,
                                         debug=0)
